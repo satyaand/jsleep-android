5e3eae72f23500c4ad5a94d4882b554b7ec2b2cf
Satya Ananda S <satyaananda65@gmail.com>
2022-11-15 21:49:44 +0700

    Revisi CS Modul 8: UI Enhancements, QoL Update, Toast Messages

--- app/src/main/java/com/satyaJSleepJS/LoginActivity.java
+++ app/src/main/java/com/satyaJSleepJS/LoginActivity.java
@@ -6,28 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView register = findViewById(R.id.textViewRegisterNow);
-        register.setOnClickListener(new View.OnClickListener() {
+        TextView logonButton = findViewById(R.id.buttonEnter);
+        logonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(move);
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
+        TextView registerClick = findViewById(R.id.textRegisterClick);
+        registerClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+    public void toastMessage(String msg){
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
 
-        TextView logged = findViewById(R.id.loginButton);
-        logged.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent enter = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(enter);
-            }
-        });
+    public void logButtonClicked(View view){
+        toastMessage("Login Confirmed!");
     }
 }
--- app/src/main/java/com/satyaJSleepJS/RegisterActivity.java
+++ app/src/main/java/com/satyaJSleepJS/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.satyaJSleepJS;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    public void toastMessage(String msg){
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void registerButtonClicked(View view){
+        toastMessage("Registration successful!");
     }
 }
